9c330aead893e5e30fa7778053ebd3c5688dff44
NayoungKim1212 <kkd04250@naver.com>
2023-07-10 13:33:47 +0900

    상품 폴더 설계

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -8,6 +8,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// One ProductFolder : Many
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
@@ -37,6 +41,9 @@ public class Product extends Timestamped {
     @ManyToOne(fetch = FetchType.LAZY) // 회원 정보가 정말로 필요할 때만 조회
     @JoinColumn(name = "user_id", nullable = false) // 상품을 등록할 때 무조건 회원만 상품 등록 가능
     private User user;
+
+    @OneToMany(mappedBy = "product") // 양방향 관계로 걸려있기 때문에 mappedBy FK 주인(속상값은 타겟팅이 되는 Entity 필드) 설정
+    private List<ProductFolder> productFolderList = new ArrayList<>();
 
     public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
